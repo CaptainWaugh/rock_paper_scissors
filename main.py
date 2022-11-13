5e476d9a9c670f773b31c6bf6fcd959c42530ba0
CaptainWaugh <117781398+CaptainWaugh@users.noreply.github.com>
2022-11-13 11:41:53 -0500

    refactor to get rid of duplicated code

--- main.py
+++ main.py
@@ -1,13 +1,76 @@
+def show_object(num: number):
+    if 0 == num:
+        basic.show_icon(IconNames.TARGET)
+    elif 1 == num:
+        basic.show_icon(IconNames.SQUARE)
+    else:
+        basic.show_leds("""
+            # . . . #
+                        . # . # .
+                        . . # . .
+                        # # . # #
+                        # # . # #
+        """)
+
 def on_button_pressed_a():
     global micro_object
-    micro_object = randint(0, 3)
+    micro_object = randint(0, 2)
+    show_object(micro_object)
     if 2 == micro_object:
         music.play_tone(392, music.beat(BeatFraction.DOUBLE))
         basic.show_icon(IconNames.YES)
+    elif 0 == micro_object:
+        basic.show_leds("""
+            . . . . .
+                        # # # # #
+                        . . . . .
+                        # # # # #
+                        . . . . .
+        """)
     else:
         music.play_tone(185, music.beat(BeatFraction.WHOLE))
         basic.show_icon(IconNames.NO)
 input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_ab():
+    global micro_object
+    micro_object = randint(0, 2)
+    show_object(micro_object)
+    if 1 == micro_object:
+        music.play_tone(392, music.beat(BeatFraction.DOUBLE))
+        basic.show_icon(IconNames.YES)
+    elif 2 == micro_object:
+        basic.show_leds("""
+            . . . . .
+                        # # # # #
+                        . . . . .
+                        # # # # #
+                        . . . . .
+        """)
+    else:
+        music.play_tone(185, music.beat(BeatFraction.WHOLE))
+        basic.show_icon(IconNames.NO)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    global micro_object
+    micro_object = randint(0, 2)
+    show_object(micro_object)
+    if 0 == micro_object:
+        music.play_tone(392, music.beat(BeatFraction.DOUBLE))
+        basic.show_icon(IconNames.YES)
+    elif 1 == micro_object:
+        basic.show_leds("""
+            . . . . .
+                        # # # # #
+                        . . . . .
+                        # # # # #
+                        . . . . .
+        """)
+    else:
+        music.play_tone(185, music.beat(BeatFraction.WHOLE))
+        basic.show_icon(IconNames.NO)
+input.on_button_pressed(Button.B, on_button_pressed_b)
 
 micro_object = 0
 object2 = 0
--- main.ts
+++ main.ts
@@ -16,48 +16,23 @@ function show_object (num: number) {
 input.onButtonPressed(Button.A, function () {
     micro_object = randint(0, 2)
     show_object(micro_object)
-    if (2 == micro_object) {
-        music.playTone(392, music.beat(BeatFraction.Double))
-        basic.showIcon(IconNames.Yes)
-    } else if (0 == micro_object) {
-        basic.showLeds(`
-            . . . . .
-            # # # # #
-            . . . . .
-            # # # # #
-            . . . . .
-            `)
-    } else {
-        music.playTone(185, music.beat(BeatFraction.Whole))
-        basic.showIcon(IconNames.No)
-    }
+    winlosetie(micro_object, 2, 0)
 })
 input.onButtonPressed(Button.AB, function () {
     micro_object = randint(0, 2)
     show_object(micro_object)
-    if (1 == micro_object) {
-        music.playTone(392, music.beat(BeatFraction.Double))
-        basic.showIcon(IconNames.Yes)
-    } else if (2 == micro_object) {
-        basic.showLeds(`
-            . . . . .
-            # # # # #
-            . . . . .
-            # # # # #
-            . . . . .
-            `)
-    } else {
-        music.playTone(185, music.beat(BeatFraction.Whole))
-        basic.showIcon(IconNames.No)
-    }
+    winlosetie(micro_object, 1, 2)
 })
 input.onButtonPressed(Button.B, function () {
     micro_object = randint(0, 2)
     show_object(micro_object)
-    if (0 == micro_object) {
+    winlosetie(micro_object, 0, 1)
+})
+function winlosetie (micro: number, win: number, tie: number) {
+    if (win == micro) {
         music.playTone(392, music.beat(BeatFraction.Double))
         basic.showIcon(IconNames.Yes)
-    } else if (1 == micro_object) {
+    } else if (tie == micro) {
         basic.showLeds(`
             . . . . .
             # # # # #
@@ -69,7 +44,7 @@ input.onButtonPressed(Button.B, function () {
         music.playTone(185, music.beat(BeatFraction.Whole))
         basic.showIcon(IconNames.No)
     }
-})
+}
 let micro_object = 0
 let object = 0
 micro_object = 0
